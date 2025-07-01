1d7c26f302d96c6b2e770d91a4d3b04156fdf9bf
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-07-01 19:55:43 +0530

    initail-setUp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='w-full place-content-center bg-green-200'>
+      <h1 className='font-extrabold text-blue-600 underline place-content-center w-[100%]'>Hello World!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
