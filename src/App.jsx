fc680571e11a484f3ebcd8afd4b82d81d40152bd
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-08-03 21:49:41 +0530

    vercel-analytics-add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { DataProvider } from './context/DataContest';
 import Layout from './layout/Layout';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
       <DataProvider>
         <Layout />
       </DataProvider>
+      <Analytics />
     </Router>
   )
 }
